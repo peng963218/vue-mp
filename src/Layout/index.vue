5ef5e07d1291e9b548216a65c53ce1843c20a830
peng963218 <1628149887@qq.com>
2020-05-20 00:35:06 +0800

    table自适应屏幕高度

--- src/Layout/index.vue
+++ src/Layout/index.vue
@@ -1,108 +1,110 @@
 <template>
-    <div class="mp_layout">
-        <div class="bg-top">
-            <bgTop/>
-        </div>
-        <div class="Main-content clearfix">
-            <div class="menu-left fl">
-                <MenuList :menu-list="MenuList"/>
-            </div>
-            <div class="template-right fr">
-                <div class="nav-bar">
-                    <NavBar/>
-                </div>
-                <div class="mp_content">
-                    <router-view/>
-                </div>
-                <div class="pages_footer">
-                    <PagesFooter/>
-                </div>
-            </div>
-        </div>
+  <div class="mp_layout">
+    <div class="bg-top">
+      <bgTop/>
     </div>
+    <div class="Main-content clearfix">
+      <div class="menu-left fl">
+        <MenuList :menu-list="MenuList"/>
+      </div>
+      <div class="template-right fr">
+        <div class="nav-bar">
+          <NavBar/>
+        </div>
+        <div class="mp_content">
+          <router-view/>
+        </div>
+        <!-- <div class="pages_footer">
+             <PagesFooter/>
+         </div> -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import bgTop from './components/bg-top'
-    import MenuList from './components/menu-list'
-    import NavBar from './components/nav-bar'
-    import PagesFooter from './components/Pages-footer'
-    import {mapGetters, mapActions} from 'vuex'
+  import bgTop from './components/bg-top'
+  import MenuList from './components/menu-list'
+  import NavBar from './components/nav-bar'
+  import PagesFooter from './components/Pages-footer'
+  import {mapGetters, mapActions} from 'vuex'
 
-    export default {
-        name: "Layout",
-        data() {
-            return {}
-        },
-        watch: {
-            '$route'(to) {
-                this.setBgTop({name: to.name, meta: to.meta})
-            }
-        },
-        created() {
-            // this.routerName = this.setBgTop.name //一进项目的时候就赋值，这样就不会一进项目出现菜单列表空白
-        },
-        methods: {
-            ...mapActions(['setBgTop'])
-        },
-        components: {
-            bgTop,
-            MenuList,
-            NavBar,
-            PagesFooter
-        },
-        computed: {
-            ...mapGetters(['MenuList'])
-        },
-        mounted() {
-            let to = this.$route
-            this.setBgTop({name: to.name, meta: to.meta})
-        },
-        destroyed() {
-        }
+  export default {
+    name: "Layout",
+    data() {
+      return {}
+    },
+    watch: {
+      '$route'(to) {
+        this.setBgTop({name: to.name, meta: to.meta})
+      }
+    },
+    created() {
+      // this.routerName = this.setBgTop.name //一进项目的时候就赋值，这样就不会一进项目出现菜单列表空白
+    },
+    methods: {
+      ...mapActions(['setBgTop'])
+    },
+    components: {
+      bgTop,
+      MenuList,
+      NavBar,
+      PagesFooter
+    },
+    computed: {
+      ...mapGetters(['MenuList'])
+    },
+    mounted() {
+      let to = this.$route
+      this.setBgTop({name: to.name, meta: to.meta})
+    },
+    destroyed() {
     }
+  }
 </script>
 
 <style scoped lang="scss">
-    .mp_layout {
-        width: 100%;
-        height: 100vh;
-        background: #F4F4F4;
+  .mp_layout {
+    width: 100%;
+    height: 100vh;
+    background: #F4F4F4;
 
-        .bg-top {
-            position: relative;
-            z-index: 1000;
-            /*border-bottom: 1px solid #e8e8e8;*/
-        }
-
-        .Main-content {
-            width: 100%;
-            height: calc(100vh - 60px);
-
-            .menu-left {
-                background-color: #3C3C46;
-                height: 100%;
-                width: 220px;
-            }
-
-            .template-right {
-                height: 100%;
-                width: calc(100% - 220px);
-                background-color: #ffffff;
-
-                .nav-bar {
-                    height: 56px;
-                }
-
-                .mp_content {
-                    /*height: 100vh;*/
-                }
-
-                .pages_footer {
-                    height: 56px;
-                }
-
-            }
-        }
+    .bg-top {
+      position: relative;
+      z-index: 1000;
+      height: 60px;
+      /*border-bottom: 1px solid #e8e8e8;*/
     }
+
+    .Main-content {
+      width: 100%;
+      height: calc(100vh - 60px);
+
+      .menu-left {
+        background-color: #3C3C46;
+        height: 100%;
+        width: 220px;
+      }
+
+      .template-right {
+        height: 100%;
+        width: calc(100% - 220px);
+        background-color: #ffffff;
+
+        .nav-bar {
+          height: 56px;
+        }
+
+        .mp_content {
+          height: calc(100% - 56px);
+          overflow: auto;
+        }
+
+        /*.pages_footer {
+          height: 56px;
+        }*/
+
+      }
+    }
+  }
 </style>
--- src/components/MpTable/table.vue
+++ src/components/MpTable/table.vue
@@ -1,179 +1,192 @@
 <template>
-    <div class="base_table">
-        <el-table
-                :data="data"
-                highlight-current-row
-                style="width: 100%"
-                :border="border"
-                :key="tableKey"
-                :max-height="maxHeight"
-                ref="MpTable"
+  <div class="base_table">
+    <el-table
+      :data="data"
+      highlight-current-row
+      style="width: 100%"
+      :border="border"
+      :key="tableKey"
+      :max-height="getMaxHeight"
+      ref="MpTable"
+    >
+      <template v-for="(row, i) of title">
+        <el-table-column
+          :type="ColumnType"
+          width="40"
+          :fixed="row.fixed"
+          :key="i"
+          v-if="row.type === 'selection'"
+        />
+        <el-table-column
+          v-else
+          :key="i"
+          :prop="row.key"
+          :label="row.title"
+          :width="row.width"
+          :min-width="row.minWidth"
+          :fixed="row.fixed"
+          show-overflow-tooltip
         >
-            <template v-for="(row, i) of title">
-                <el-table-column
-                        :type="ColumnType"
-                        width="40"
-                        :fixed="row.fixed"
-                        :key="i"
-                        v-if="row.type === 'selection'"
-                />
-                <el-table-column
-                        v-else
-                        :key="i"
-                        :prop="row.key"
-                        :label="row.title"
-                        :width="row.width"
-                        :min-width="row.minWidth"
-                        :fixed="row.fixed"
-                        show-overflow-tooltip
-                >
-                    <template slot="header" slot-scope="scope">
-                        <span v-if="!row.headSlot">{{scope.column.label}}</span>
-                        <render-dom
-                                v-else
-                                :render="row.headSlot"
-                                :index="scope.$index"
-                                :column="scope.column"
-                                :row="scope.column"
-                        />
-                    </template>
-                    <template slot-scope="scope">
-                        <span v-if="!row.render">{{ scope.row[row.key] }}</span>
-                        <render-dom
-                                v-else
-                                :render="row.render"
-                                :index="scope.$index"
-                                :column="scope.column"
-                                :row="scope.row"
-                        />
-                    </template>
-                </el-table-column>
-            </template>
-        </el-table>
+          <template slot="header" slot-scope="scope">
+            <span v-if="!row.headSlot">{{scope.column.label}}</span>
+            <render-dom
+              v-else
+              :render="row.headSlot"
+              :index="scope.$index"
+              :column="scope.column"
+              :row="scope.column"
+            />
+          </template>
+          <template slot-scope="scope">
+            <span v-if="!row.render">{{ scope.row[row.key] }}</span>
+            <render-dom
+              v-else
+              :render="row.render"
+              :index="scope.$index"
+              :column="scope.column"
+              :row="scope.row"
+            />
+          </template>
+        </el-table-column>
+      </template>
+    </el-table>
 
-        <!--        <div class="ld_base_table_page">-->
-        <!--            <el-pagination-->
-        <!--                    small-->
-        <!--                    layout="prev, pager, next, total, sizes,  jumper"-->
-        <!--                    :page-size="pageSize"-->
-        <!--                    :total="total"-->
-        <!--                    :page-sizes="pageSizes"-->
-        <!--                    @size-change="sizeChange"-->
-        <!--                    @current-change="onPage"-->
-        <!--                    :current-page="page"-->
-        <!--            />-->
-        <!--        </div>-->
+    <div class="base_table_page" v-if="isPage">
+      <el-pagination
+        small
+        layout="prev, pager, next, total, sizes,  jumper"
+        :page-size="pageSize"
+        :total="total"
+        :page-sizes="pageSizes"
+        @size-change="sizeChange"
+        @current-change="onPage"
+        :current-page="page"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-    import renderDom from './render_dom'
+  import renderDom from './render_dom'
 
-    export default {
-        name: 'MpTable',
-        data() {
-            return {
-                tableKey: null
-            }
-        },
-        props: {
-            title: {
-                type: Array,
-                default() {
-                    return []
-                }
-            }, // 表格 header
-            data: {
-                type: Array,
-                default() {
-                    return []
-                }
-            }, // 表格数据
-            ColumnType: {
-                type: String,
-                default: 'index'
-            }, // 对应列的类型
-            maxHeight: {
-                type: [String, Number],
-                default: '1400'
-            }, // 最大高度
-            border: {
-                type: Boolean,
-                default: true
-            }, // 表格边框
-            page: {
-                type: Number,
-                default: 1
-            }, // 当前页面序号
-            pageSizes: {
-                type: Array,
-                default() {
-                    return [10, 20]
-                }
-            }, // 展示页面条数设置
-            total: {
-                type: Number,
-                default: 0
-            }, // 总条数
-            pageSize: {
-                type: Number,
-                default: 10
-            } // 每页展示数
-        },
-        watch: {
-            data: {
-                handler() {
-                    this.tableKey = new Date().getTime()
-                    if (this.$refs.MpTable) {
-                        this.$refs.MpTable.bodyWrapper.scrollTop = 0
-                    }
-                },
-                deep: true
-            }
-        },
-        created() {
-            this.tableKey = new Date().getTime()
-        },
-        methods: {
-            putTableBodyWidth() {
-                if (this.$refs.MpTable) {
-                    // el-table__body
-                    this.$nextTick(() => {
-                        let dom = this.$refs.MpTable.bodyWrapper.querySelector('.el-table__body')
-
-                        if (dom) {
-                            console.log(`${dom.getBoundingClientRect().width - 15}px`)
-                            setTimeout(() => {
-                                dom.style.width = `${dom.getBoundingClientRect().width - 15}px`
-                            }, 20)
-                        }
-                        // console.log(this.$refs.ldTable.bodyWrapper.querySelector('.el-table__body').getBoundingClientRect().width)
-                    })
-                }
-            },
-            sizeChange(val) {
-                this.$emit('on-size', val)
-            },
-            onPage(val) {
-                this.$emit('on-page', val)
-            }
-        },
-        components: {
-            renderDom
-        },
-        computed: {},
-        mounted() {
-            // this.putTableBodyWidth()
-        },
-        destroyed() {
+  export default {
+    name: 'MpTable',
+    data() {
+      return {
+        tableKey: null,
+        TableMaxHeight: null
+      }
+    },
+    props: {
+      title: {
+        type: Array,
+        default() {
+          return []
         }
+      }, // 表格 header
+      data: {
+        type: Array,
+        default() {
+          return []
+        }
+      }, // 表格数据
+      ColumnType: {
+        type: String,
+        default: 'index'
+      }, // 对应列的类型
+      maxHeight: [String, Number], // 最大高度
+      border: {
+        type: Boolean,
+        default: true
+      }, // 表格边框
+      page: {
+        type: Number,
+        default: 1
+      }, // 当前页面序号
+      pageSizes: {
+        type: Array,
+        default() {
+          return [10, 20]
+        }
+      }, // 展示页面条数设置
+      total: {
+        type: Number,
+        default: 0
+      }, // 总条数
+      pageSize: {
+        type: Number,
+        default: 10
+      }, // 每页展示数
+      isPage: {
+        type: Boolean,
+        default: true
+      }
+    },
+    watch: {
+      data: {
+        handler() {
+          this.tableKey = new Date().getTime()
+          if (this.$refs.MpTable) {
+            this.$refs.MpTable.bodyWrapper.scrollTop = 0
+          }
+        },
+        deep: true
+      }
+    },
+    created() {
+      this.tableKey = new Date().getTime()
+    },
+    methods: {
+      putTableBodyWidth() {
+        if (this.$refs.MpTable) {
+          // el-table__body
+          this.$nextTick(() => {
+            let dom = this.$refs.MpTable.bodyWrapper.querySelector('.el-table__body')
+
+            if (dom) {
+              console.log(`${dom.getBoundingClientRect().width - 15}px`)
+              setTimeout(() => {
+                dom.style.width = `${dom.getBoundingClientRect().width - 15}px`
+              }, 20)
+            }
+            // console.log(this.$refs.ldTable.bodyWrapper.querySelector('.el-table__body').getBoundingClientRect().width)
+          })
+        }
+      },
+      sizeChange(val) {
+        this.$emit('on-size', val)
+      },
+      onPage(val) {
+        this.$emit('on-page', val)
+      }
+    },
+    components: {
+      renderDom
+    },
+    computed: {
+      getMaxHeight() {
+        return this.maxHeight || this.TableMaxHeight
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        if (!this.isPage) return false
+        this.TableMaxHeight = this.$el.offsetHeight - 56
+      })
+    },
+    destroyed() {
     }
+  }
 </script>
 
 <style scoped lang="scss">
-    .base_table {
-        height: calc(100vh - 173px);
-        overflow: hidden;
-        overflow-y:scroll;
+  .base_table {
+    height: 100%;
+
+    .base_table_page {
+      height: 56px;
+      display: flex;
+      align-items: center;
     }
+  }
 </style>
